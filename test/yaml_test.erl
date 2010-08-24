81d63c285b2b30c5ba832244810ab06b233c33ed
Oscar Hellström <oscar@hellstrom.st>
2010-08-24 23:05:42 +0200

    Upadet tests to describe the errors better.

--- test/yaml_test.erl
+++ test/yaml_test.erl
@@ -3,19 +3,24 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("yeml.hrl").
 
--define(TEST_FILE, "../test/test.yaml").
+-define(SIMPLE_FILE, "../test/simple.yaml").
 
-parse_test() ->
-    {ok, Bin} = file:read_file(?TEST_FILE),
+simlpe_document_test() ->
+    {ok, Bin} = file:read_file(?SIMPLE_FILE),
     State = #ye_cb_state{
         state= [doc_begin, doc_end, {comment, "End document"}],
-        doc_begin = fun(S) -> [doc_begin | T] = S#ye_cb_state.state,
-                S#ye_cb_state{state=T} end,
-        doc_end = fun(S) -> [doc_end | T] = S#ye_cb_state.state,
-                S#ye_cb_state{state=T} end,
-        comment = fun(S, Comment) -> [{comment, Comment} | T] =
-                S#ye_cb_state.state,
-                S#ye_cb_state{state=T} end
+        doc_begin = fun(S) ->
+                ?assertMatch(doc_begin, hd(S#ye_cb_state.state)),
+                S#ye_cb_state{state=tl(S#ye_cb_state.state)}
+            end,
+        doc_end = fun(S) ->
+                ?assertMatch(doc_end, hd(S#ye_cb_state.state)),
+                S#ye_cb_state{state = tl(S#ye_cb_state.state)}
+            end,
+        comment = fun(S, Comment) ->
+                ?assertMatch({comment, Comment}, hd(S#ye_cb_state.state)),
+                S#ye_cb_state{state=tl(S#ye_cb_state.state)}
+            end
     },
     check_result(ye_stream_parse:bin(Bin, State)).
 
