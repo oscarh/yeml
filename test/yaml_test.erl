bfa0728adc465af0dea3493c99271f66dcbed7d7
Anton Lofgren <anton.lofgren@gmail.com>
2010-08-24 15:35:07 +0200

    fixed indentation

--- test/yaml_test.erl
+++ test/yaml_test.erl
@@ -6,12 +6,12 @@
 -define(TEST_FILE, "../test/test.yaml").
 
 parse_test() ->
-        {ok, Bin} = file:read_file(?TEST_FILE),
-        State = #ye_cb_state{
-                doc_begin = fun(S) -> io:format("Document begin"), S end,
-                doc_end = fun(S) -> io:format("Document end"), S end
-        },
-        ?_assertMatch(ok, check_result(ye_stream_parse:bin(Bin, State))).
+    {ok, Bin} = file:read_file(?TEST_FILE),
+    State = #ye_cb_state{
+        doc_begin = fun(S) -> io:format("Document begin"), S end,
+        doc_end = fun(S) -> io:format("Document end"), S end
+    },
+    ?_assertMatch(ok, check_result(ye_stream_parse:bin(Bin, State))).
 
 check_result(_) ->
-        ok.
+    ok.
